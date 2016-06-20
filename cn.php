45eb593e6107df96ceb8c8d3f59ddf8f730e9b08
FBBYN <FBBYN@users.noreply.github.com>
2016-06-20 13:04:45 +0800

    Update cn.php

--- cn.php
+++ cn.php
@@ -1,5 +1,5 @@
 <?php  
- $conn = mysql_connect("127.0.0.1","root","mitra") or die("Error bro!".mysql_error());
- mysql_select_db("test",$conn) or die("Error  bro!".mysql_error());
+ $conn = mysql_connect("127.0.0.1","root","<password>") or die("Error".mysql_error());
+ mysql_select_db("test",$conn) or die("Error".mysql_error());
  mysql_query("set names utf8");
 ?>
